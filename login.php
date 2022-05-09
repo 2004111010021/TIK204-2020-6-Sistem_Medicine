b96b72bbd8b427a061373472338e87f7d6fa765f
Muhammad Nouval <101517320+MuhammadNouval64@users.noreply.github.com>
2022-05-09 11:19:18 +0700

    Add files via upload

--- login.php
+++ login.php
@@ -6,13 +6,14 @@
 <head>
        <title>WEB MEDICINE</title>
 </head>
-<body background="images.jpg">
+<body background="pills-png-1.png">
 
 <h1>LOGIN</h1>
 		<a href="sign up.php" target="_SELF">
-		<button class="btn fifth">register</button></a>
+		<button class="btn fifth">REGISTER</button></a>
+		<background position: center; margin-left: auto; margin-right: auto;>
 
-<table align="center" border="1">
+<table align="center" border="4" cellpadding ="15">
     <tr>
 	<td>
        <form action="login-proses.php" method="post">
